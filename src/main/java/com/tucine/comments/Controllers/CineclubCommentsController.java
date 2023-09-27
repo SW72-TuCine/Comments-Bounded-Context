46dda6d969b70c0b3fcf9729a20b652f65da0423
GiulianoMendez <u201824528@upc.edu.pe>
2023-09-27 09:05:55 -0500

    fix: added cineclub comments controller

--- src/main/java/com/tucine/comments/Controllers/CineclubCommentsController.java
+++ src/main/java/com/tucine/comments/Controllers/CineclubCommentsController.java
@@ -1,4 +1,60 @@
 package com.tucine.comments.Controllers;
 
+import com.tucine.comments.Models.CineclubComments;
+import com.tucine.comments.Services.CineclubCommentsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/cineclub-comments")
 public class CineclubCommentsController {
+    private final CineclubCommentsService cineclubCommentsService;
+
+    @Autowired
+    public CineclubCommentsController(CineclubCommentsService cineclubCommentsService) {
+        this.cineclubCommentsService = cineclubCommentsService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CineclubComments>> getAllCineclubComments() {
+        List<CineclubComments> cineclubComments = cineclubCommentsService.getAllCineclubComments();
+        return new ResponseEntity<>(cineclubComments, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CineclubComments> getCineclubCommentById(@PathVariable Long id) {
+        CineclubComments cineclubComment = cineclubCommentsService.getCineclubCommentById(id);
+        if (cineclubComment != null) {
+            return new ResponseEntity<>(cineclubComment, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<CineclubComments> createCineclubComment(@RequestBody CineclubComments cineclubComment) {
+        CineclubComments createdComment = cineclubCommentsService.createCineclubComment(cineclubComment);
+        return new ResponseEntity<>(createdComment, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CineclubComments> updateCineclubComment(@PathVariable Long id, @RequestBody CineclubComments cineclubComment) {
+        CineclubComments updatedComment = cineclubCommentsService.updateCineclubComment(id, cineclubComment);
+        if (updatedComment != null) {
+            return new ResponseEntity<>(updatedComment, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCineclubComment(@PathVariable Long id) {
+        cineclubCommentsService.deleteCineclubComment(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
